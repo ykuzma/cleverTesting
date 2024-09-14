8cc5f6659fad6172b19fa3680515d5e238394e17
Yury Kuzma <vterkin1388@gmail.com>
2024-09-14 22:06:03 +0300

    add JPA annotation in CarEntity, add jacoco-plugin

--- src/main/java/by/clevertec/testCar/entity/CarEntity.java
+++ src/main/java/by/clevertec/testCar/entity/CarEntity.java
@@ -1,6 +1,9 @@
 package by.clevertec.testCar.entity;
 
 import by.clevertec.testCar.common.CarType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,10 +13,16 @@ import java.util.UUID;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 public class CarEntity {
+    @Id
     private UUID id;
+    @Column
     private long vehicleMileage;
+    @Column
     private int doorCount;
+    @Column
     private int trunkCapacity;
+    @Column
     private CarType carType;
 }
